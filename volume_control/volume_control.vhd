fba6343aada2bb82f4efce62dc646e4dec949e24
Tristan Penman <tristan@tristanpenman.com>
2020-08-09 18:51:05 +1000

    Make volume_control more consistent with volume_control_16

--- volume_control/volume_control.vhd
+++ volume_control/volume_control.vhd
@@ -41,21 +41,22 @@ architecture Behavioral of volume_control is
   end component;
 
   signal addr    : STD_LOGIC_VECTOR(9 downto 0);
-  signal counter : STD_LOGIC_VECTOR(26 downto 0);
+  signal counter : STD_LOGIC_VECTOR(21 downto 0);
   signal data1   : STD_LOGIC_VECTOR(7 downto 0);
   signal data2   : STD_LOGIC_VECTOR(11 downto 0);
   signal volume  : STD_LOGIC_VECTOR(3 downto 0) := "1111";
 
 begin
 
-  -- avoid burning out the leds
-  set_anodes: process(clk, counter)
+  anodes(3 downto 1) <= "111";
+
+  set_anodes: process(clk)
   begin
     if rising_edge(clk) then
-      if counter(18 downto 14) = "11111" then
-        anodes <= "1110";
+      if counter(7 downto 0) = "00000000" then
+        anodes(0) <= '0';
       else
-        anodes <= "1111";
+        anodes(0) <= '1';
       end if;
     end if;
   end process;
@@ -63,12 +64,35 @@ begin
   set_volume: process(counter(21))
   begin
     if rising_edge(counter(21)) then
-      if NOT(JOY_R = '1') AND JOY_L = '1' AND NOT(volume = "1111") then
+      if not(JOY_R = '1') and JOY_L = '1' and not(volume = "1111") then
         volume <= volume + 1;
-      elsif NOT(JOY_L = '1') AND JOY_R = '1' AND NOT(volume = "0000") then
+      elsif not(JOY_L = '1') and JOY_R = '1' and not(volume = "0000") then
         volume <= volume - 1;
       end if;
     end if;
+  end process;
+
+  update_segments: process(volume)
+  begin
+    -- Map the current digit to the seven segment display
+    case volume is
+      when "0000" => segs <= "1000000";
+      when "0001" => segs <= "1111001";
+      when "0010" => segs <= "0100100";
+      when "0011" => segs <= "0110000";
+      when "0100" => segs <= "0011001";
+      when "0101" => segs <= "0010010";
+      when "0110" => segs <= "0000010";
+      when "0111" => segs <= "1111000";
+      when "1000" => segs <= "0000000";
+      when "1001" => segs <= "0011000";
+      when "1010" => segs <= "0001000";
+      when "1011" => segs <= "0000011";
+      when "1100" => segs <= "1000110";
+      when "1101" => segs <= "0100001";
+      when "1110" => segs <= "0000110";
+      when others => segs <= "0001110";
+    end case;
   end process;
 
   process(clk)
@@ -105,28 +129,5 @@ begin
       pulse => AUDIO
     );
 
-  update_segments: process(volume)
-  begin
-    -- Map the current digit to the seven segment display
-    case volume is
-      when "0000" => segs <= "1000000";
-      when "0001" => segs <= "1111001";
-      when "0010" => segs <= "0100100";
-      when "0011" => segs <= "0110000";
-      when "0100" => segs <= "0011001";
-      when "0101" => segs <= "0010010";
-      when "0110" => segs <= "0000010";
-      when "0111" => segs <= "1111000";
-      when "1000" => segs <= "0000000";
-      when "1001" => segs <= "0011000";
-      when "1010" => segs <= "0001000";
-      when "1011" => segs <= "0000011";
-      when "1100" => segs <= "1000110";
-      when "1101" => segs <= "0100001";
-      when "1110" => segs <= "0000110";
-      when others => segs <= "0001110";
-    end case;
-  end process;
- 
 end Behavioral;
 
